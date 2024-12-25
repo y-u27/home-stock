4dd13ad5d28f09398b53c7094d00b53cd6455db1
Atd27 <y518a480@gmail.com>
2024-12-26 00:33:55 +0900

    🎉 「あれ、あったっけ？」作成開始

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Providers } from "./provider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,8 +12,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="ja">
+      <body>
+        <Providers></Providers>
         {children}
       </body>
     </html>
