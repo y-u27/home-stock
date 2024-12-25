2f000e35a83155efa37eda33c87988b8516b43e3
Atd27 <y518a480@gmail.com>
2024-12-26 01:28:41 +0900

    ヘッダー作成中

---
+++ app/components/Header.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "@chakra-ui/react";
+import React from "react";
+
+const Header = () => {
+  return (
+    <Box bgColor="yellow.200" height="50px">
+      <Text justifyContent="center" textAlign="center" fontSize="24px" color="green.500" fontWeight="bold">あれ、あったっけ？</Text>
+    </Box>
+  );
+};
+
+export default Header;
