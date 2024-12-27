2982596a32fc67b1bb2acefe548b5c16e7d457e2
Atd27 <y518a480@gmail.com>
2024-12-28 00:27:12 +0900

    メインページコンポーネント実装（途中）

---
+++ app/manage/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainPage from '../components/MainPage'
+
+const ManagePage = () => {
+  return (
+    <>
+      <MainPage/>
+    </>
+  )
+}
+
+export default ManagePage
