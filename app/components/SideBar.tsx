a619ba6021fdb862292327a842576a2c0e6035e2
Atd27 <y518a480@gmail.com>
2024-12-28 00:26:48 +0900

    サイドバーコンポーネント作成中

---
+++ app/components/SideBar.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { HamburgerIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  IconButton,
+  Input,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+const SideBar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Box position="fixed" right="96%" top="5px" zIndex={1000}>
+        <IconButton
+          icon={<HamburgerIcon />}
+          onClick={onOpen}
+          aria-label="Open menu"
+          bg="transparent"
+          fontSize="24px"
+          _hover={{ bg: "gray.200" }}
+        />
+      </Box>
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose} size="sm">
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>メニュー</DrawerHeader>
+          <DrawerBody>
+            <Text>登録一覧</Text>
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button mr={3}>ログアウト</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default SideBar;
