959bf251ef6c3f1478a70521db34e7846d8979af
Atd27 <y518a480@gmail.com>
2024-12-28 00:26:15 +0900

    ヘッダーコンポーネント作成中

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,10 +1,22 @@
-import { Box, Text } from "@chakra-ui/react";
+"use client";
+
+import { Box, Button, Text } from "@chakra-ui/react";
 import React from "react";
+import SideBar from "./SideBar";
 
 const Header = () => {
   return (
     <Box bgColor="yellow.200" height="50px">
-      <Text justifyContent="center" textAlign="center" fontSize="24px" color="green.500" fontWeight="bold">あれ、あったっけ？</Text>
+      <Text
+        justifyContent="center"
+        textAlign="center"
+        fontSize="24px"
+        color="green.500"
+        fontWeight="bold"
+      >
+        あれ、あったっけ？
+      </Text>
+      <SideBar />
     </Box>
   );
 };
