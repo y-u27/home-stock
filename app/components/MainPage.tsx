88a9fd93975461d9d8520919b8722e05f22ddb2a
Atd27 <y518a480@gmail.com>
2025-01-05 23:41:33 +0900

    登録・検索フィールド追加

--- app/components/MainPage.tsx
+++ app/components/MainPage.tsx
@@ -1,12 +1,19 @@
-import { Box, Input, Text } from "@chakra-ui/react";
+import { Box, Input, Spacer, Text } from "@chakra-ui/react";
 import React from "react";
 
 const MainPage = () => {
   return (
     <>
-      <Box>
-        <Text>登録</Text>
-        <Input/>
+      <Box display="flex" justifyContent="center" pt="200px">
+        <Box pl="30%">
+          <Text fontSize="20px" textAlign="center" textShadow='2px 2px #4FD1C5' m='5'>登録</Text>
+          <Input />
+        </Box>
+        <Spacer />
+        <Box pr="30%">
+          <Text fontSize="20px" textAlign="center" textShadow='2px 2px #F6AD55' m='5'>検索</Text>
+          <Input />
+        </Box>
       </Box>
     </>
   );
