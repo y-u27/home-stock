5dac27ed4e606d11d653a7c5a5c4be24a5b96847
Atd27 <y518a480@gmail.com>
2025-03-15 18:52:13 +0900

    軽微修正

---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import Login from "../components/Login";
+
+const loginPage = () => {
+  return (
+    <Box>
+      <Login />
+    </Box>
+  );
+};
+
+export default loginPage;
