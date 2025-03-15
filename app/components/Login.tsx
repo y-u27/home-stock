992a4225967e19bb56a971e96e087f142e43f08f
Atd27 <y518a480@gmail.com>
2025-03-15 18:51:06 +0900

    「説明」削除

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -3,15 +3,10 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 const Login = () => {
   return (
     <>
-      <Box display="flex" justifyContent="center" >
-        <Box>説明1</Box>
-        <Box>説明2</Box>
-        <Box>説明3</Box>
-      </Box>
       <Flex height="100vh" alignItems="center" justifyContent="center">
-        {/* <Flex direction="column" background="gray.100" padding={12} rounded={6}>
+        <Flex direction="column" background="gray.100" padding={12} rounded={6}>
           <Text>ログイン</Text>
-        </Flex> */}
+        </Flex>
       </Flex>
     </>
   );
