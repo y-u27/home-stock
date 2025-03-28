5e1479deaa5c44ac98a5fab2917b2d98f46b08be
Atd27 <y518a480@gmail.com>
2025-03-28 23:55:41 +0900

    アプリの概要スライド完成

--- app/components/Slider.tsx
+++ app/components/Slider.tsx
@@ -4,11 +4,33 @@ import { Box, Button, HStack, Text } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import React, { useState } from "react";
 
+type slidesType = {
+  operation1: string;
+  operation2: string;
+  operation3: string;
+};
+
 // motionをChakra UIのBoxでラップ
 const MotionHStack = motion(HStack);
 
 // スライドデータ
-const slides = ["説明1", "説明2", "説明3"];
+const slides: slidesType[] = [
+  {
+    operation1: "〜家の在庫管理アプリ〜",
+    operation2: "今家にないものなどを登録して探すことができます",
+    operation3: "",
+  },
+  {
+    operation1: "【登録】",
+    operation2: "[登録]に登録したいものの名前を入力",
+    operation3: "個数も登録することができます",
+  },
+  {
+    operation1: "【検索】",
+    operation2: "[検索]にて登録したものを入力",
+    operation3: "登録したものを個数と併せて確認できます",
+  }
+];
 
 const Slider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -25,43 +47,54 @@ const Slider = () => {
 
   return (
     <>
-      <Box>
+      <Box
+        h="100vh"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        position="relative"
+      >
         <MotionHStack
-          w="300%"
-          h="200px"
-          bg="cyan.400"
-          border="cyan.700"
+          w="500%"
+          h="300px"
+          bgGradient='linear(to-r, cyan.200, yellow.300)'
+          borderRadius="30px"
           alignContent="center"
           justifyContent="center"
           boxShadow="md"
-          position="relative"
+          position="absolute"
+          bottom="45%"
           key={currentIndex}
           initial={{ opacity: 0, x: 50 }}
           animate={{ opacity: 1, x: 0 }}
           exit={{ opacity: 0, x: -50 }}
           transition={{ duration: 0.5 }}
         >
-          <Text color="white" fontSize="xl">
-            {slides[currentIndex]}
-          </Text>
+          <Box>
+            <Text color="black" fontSize="xl">
+              {slides[currentIndex].operation1}
+            </Text>
+            <Text color="black" fontSize="xl">
+              {slides[currentIndex].operation2}
+            </Text>
+            <Text color="black" fontSize="xl">
+              {slides[currentIndex].operation3}
+            </Text>
+          </Box>
         </MotionHStack>
-      </Box>
-
-      <Box>
-        <HStack spacing={4} mt="50%">
+        <HStack spacing={4} mt="120%">
           <Button onClick={prevSlide}>前へ</Button>
           <Button onClick={nextSlide}>次へ</Button>
         </HStack>
       </Box>
-
-      <Box>
-        <HStack spacing={2} mt={4}>
+      <Box position="absolute" bottom="28%">
+        <HStack spacing={2}>
           {slides.map((_, index) => (
             <Box
               key={index}
               w={currentIndex === index ? "12px" : "8px"}
               h={currentIndex === index ? "12px" : "8px"}
-              bg={currentIndex === index ? "teal.500" : "gray.300"}
+              bg={currentIndex === index ? "cyan.500" : "gray.300"}
               borderRadius="50%"
               transition="0.3s"
             />
